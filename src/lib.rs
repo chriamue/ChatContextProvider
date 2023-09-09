166bf53e0f6a69e3a7df9fe8b90af11c5e9b6618
Christian M <chriamue@gmail.com>
2023-09-09 17:56:07 +0200

    :sparkles: adds openapi

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use rocket::get;
+use rocket::{get, serde::json::Json};
 
 use rocket::fs::NamedFile;
+use rocket_okapi::openapi;
+use schemars::JsonSchema;
+use serde::Serialize;
 use std::path::{Path, PathBuf};
 
 #[get("/<file..>")]
@@ -8,8 +11,25 @@ pub async fn assets(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("assets/").join(file)).await.ok()
 }
 
-
 #[get("/")]
 pub fn world() -> &'static str {
     "Hello, world!"
+}
+
+#[derive(Serialize, JsonSchema)]
+pub struct Project {
+    name: String,
+    description: String,
+    git_url: String,
+}
+
+#[openapi]
+#[get("/current-project")]
+pub fn current_project() -> Json<Project> {
+    Json(Project {
+        name: "ChatContextProvider".to_string(),
+        description: "A ChatGPT plugin to provide contextual information about GitHub projects."
+            .to_string(),
+        git_url: "https://github.com/chriamue/chat-context-provider.git".to_string(),
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use ccp::{assets, world};
+
+use rocket_okapi::{openapi_get_routes, swagger_ui::*};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![assets, world])
+    rocket::build()
+        .mount("/", routes![assets, world])
+        .mount("/", openapi_get_routes![ccp::current_project])
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
 }
